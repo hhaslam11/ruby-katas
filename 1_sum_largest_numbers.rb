e9bc8f1fa5c25d8dcf14f794c4c8207aecdc49af
hhaslam11 <kalebmarc@hotmail.com>
2019-12-25 00:34:19 -0800

    finish first kata

--- 1_sum_largest_numbers.rb
+++ 1_sum_largest_numbers.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 def sum_largest_numbers(data)
-  data
+  # sort from largest to smallest
+  data = data.sort { |a, b| b - a }
+
+  data[0] + data[1]
 end
 
-test_array = [1, 2, 3, 4, 5]
-
-p sum_largest_numbers test_array
+puts "#{sum_largest_numbers [1, 10]} should be 11"
+puts "#{sum_largest_numbers [1, 2, 3]} should be 5"
+puts "#{sum_largest_numbers [10, 4, 34, 6, 92, 2]} should be 126"
